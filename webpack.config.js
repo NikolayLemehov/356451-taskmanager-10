2f4b830ccd1d683b41e81a2324898f31bf25d835
Nikolay Liemiekhov <nicklemster@gmail.com>
2019-11-23 11:51:50 +0200

    Добавляет автоматизацию webpack, включает работу с модулями в ESLint

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require(`path`);
+
+module.exports = {
+  mode: `development`, // Режим сборки
+  entry: `./src/main.js`, // Точка входа приложения
+  output: {// Настройка выходного файла
+    filename: `bundle.js`,
+    // eslint-disable-next-line no-undef
+    path: path.join(__dirname, `public`)
+  },
+  devServer: {
+    // eslint-disable-next-line no-undef
+    contentBase: path.join(__dirname, `public`), // Где искать сборку
+    publicPath: `http://localhost:8080/`, // Веб адрес сборки
+    compress: true, // Сжатие
+    watchContentBase: true
+  },
+  devtool: `source-map` // Подключаем sourcemaps
+};
