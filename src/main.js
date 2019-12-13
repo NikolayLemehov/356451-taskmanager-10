1fca4b4e775effb5228d2673372788c640376604
Nikolay Liemiekhov <nicklemster@gmail.com>
2019-12-13 15:33:37 +0200

    Реализует переключение таска между состояниями

--- src/main.js
+++ src/main.js
@@ -14,6 +14,40 @@ const ShowingTasksCount = {
   BY_BUTTON: 8,
 };
 
+const renderTask = (taskListElement, task) => {
+  const onEscKeyDown = (evt) => {
+    const isEscKey = evt.key === `Escape` || evt.key === `Esc`;
+
+    if (isEscKey) {
+      replaceEditToTask();
+      document.removeEventListener(`keydown`, onEscKeyDown);
+    }
+  };
+
+  const replaceEditToTask = () => {
+    taskListElement.replaceChild(taskComponent.getElement(), taskEditComponent.getElement());
+  };
+
+  const replaceTaskToEdit = () => {
+    taskListElement.replaceChild(taskEditComponent.getElement(), taskComponent.getElement());
+  };
+
+  const taskComponent = new TaskComponent(task);
+  const editButton = taskComponent.getElement().querySelector(`.card__btn--edit`);
+
+  editButton.addEventListener(`click`, () => {
+    replaceTaskToEdit();
+    document.addEventListener(`keydown`, onEscKeyDown);
+  });
+
+  const taskEditComponent = new TaskEditComponent(task);
+  const editForm = taskEditComponent.getElement().querySelector(`form`);
+
+  editForm.addEventListener(`submit`, replaceEditToTask);
+
+  render(taskListElement, taskComponent.getElement());
+};
+
 const siteMainElement = document.querySelector(`.main`);
 const siteHeaderElement = siteMainElement.querySelector(`.main__control`);
 
@@ -21,14 +55,15 @@ render(siteHeaderElement, new SiteMenuComponent().getElement());
 
 const filters = generateFilters();
 render(siteMainElement, new FilterComponent(filters).getElement());
-render(siteMainElement, new BoardComponent().getElement());
+
+const boardComponent = new BoardComponent();
+render(siteMainElement, boardComponent.getElement());
 
 const taskListElement = siteMainElement.querySelector(`.board__tasks`);
 const tasks = generateTasks(TASK_COUNT);
 
-render(taskListElement, new TaskEditComponent(tasks[0]).getElement());
 let showingTasksCount = ShowingTasksCount.ON_START;
-tasks.slice(1, showingTasksCount).forEach((task) => render(taskListElement, new TaskComponent(task).getElement()));
+tasks.slice(0, showingTasksCount).forEach((task) => renderTask(taskListElement, task));
 
 const boardElement = siteMainElement.querySelector(`.board`);
 render(boardElement, new LoadMoreButtonComponent().getElement());
@@ -39,7 +74,7 @@ loadMoreButtonElement.addEventListener(`click`, () => {
   showingTasksCount = showingTasksCount + ShowingTasksCount.BY_BUTTON;
 
   tasks.slice(prevTasksCount, showingTasksCount)
-    .forEach((task) => render(taskListElement, new TaskComponent(task).getElement()));
+    .forEach((task) => renderTask(taskListElement, task));
 
   if (showingTasksCount >= tasks.length) {
     loadMoreButtonElement.remove();
