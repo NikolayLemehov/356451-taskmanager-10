a72d08e1e7995e0ae607b0d7a26e68749391b4bb
Nikolay Liemiekhov <nicklemster@gmail.com>
2019-12-25 20:30:40 +0200

    Выносит выносит метод _onEscKeyDown в классе TaskController, удаляет переменную taskListElement

--- src/controllers/task-controller.js
+++ src/controllers/task-controller.js
@@ -14,24 +14,14 @@ export default class TaskController {
     this._taskComponent = new TaskComponent(task);
     this._taskEditComponent = new TaskEditComponent(task);
 
-    const taskListElement = this._container;
-    const onEscKeyDown = (evt) => {
-      const isEscKey = evt.key === `Escape` || evt.key === `Esc`;
-
-      if (isEscKey) {
-        this._replaceEditToTask();
-        document.removeEventListener(`keydown`, onEscKeyDown);
-      }
-    };
-
     this._taskComponent.setEditButtonClickHandler(() => {
       this._replaceTaskToEdit();
-      document.addEventListener(`keydown`, onEscKeyDown);
+      document.addEventListener(`keydown`, this._onEscKeyDown);
     });
 
     this._taskEditComponent.setSubmitHandler(this._replaceEditToTask);
 
-    renderElement(taskListElement, this._taskComponent);
+    renderElement(this._container, this._taskComponent);
   }
 
   _replaceEditToTask() {
@@ -40,5 +30,14 @@ export default class TaskController {
 
   _replaceTaskToEdit() {
     replaceElement(this._taskEditComponent, this._taskComponent);
+  }
+
+  _onEscKeyDown(evt) {
+    const isEscKey = evt.key === `Escape` || evt.key === `Esc`;
+
+    if (isEscKey) {
+      this._replaceEditToTask();
+      document.removeEventListener(`keydown`, this._onEscKeyDown);
+    }
   }
 }
