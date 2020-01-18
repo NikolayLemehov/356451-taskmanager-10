66482b542c75de3a0362a1e2e770973a8052eccb
Nikolay Liemiekhov <nicklemster@gmail.com>
2020-01-18 16:48:12 +0200

    Добавляет удаление задачи с сервера

--- src/api.js
+++ src/api.js
@@ -38,6 +38,10 @@ export default class API {
       .then(TaskAdapterModel.parseTask);
   }
 
+  deleteTask(id) {
+    return this._load({url: `tasks/${id}`, method: Method.DELETE});
+  }
+
   _load({url, method = Method.GET, body = null, headers = new Headers()}) {
     headers.append(`Authorization`, this._authorization);
 
